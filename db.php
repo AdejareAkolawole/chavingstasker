d5aa1d5c845991a8159b9d73203993eead25a63d
Adejare Akolawole <160399719+AdejareAkolawole@users.noreply.github.com>
2025-09-11 13:05:59 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
 // Database connection using PDO
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=chavings_tasker", "root", "");
+    $pdo = new PDO("mysql:host=db.pxxl.pro:5795;dbname=db_d3eba46b", "root", "be90d1e513b38df4f8a6375365c686de");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+
 ?>
